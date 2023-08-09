7fbb2dba2d5f3d7e98eebd1c72e1a112c13541ba
Vitoria <vazevedo73@gmail.com>
2023-08-09 12:05:16 -0300

    finalizado aula02

--- aula02/app.js
+++ aula02/app.js
@@ -8,9 +8,13 @@ const forma1 = document.getElementById('forma1')
 const forma2 = document.getElementById('forma2')
 const forma3 = document.getElementById('forma3')
 const forma4 = document.getElementById('forma4')
+const forma5 = document.getElementById('forma5')
+const forma6 = document.getElementById('forma6')
+const forma7 = document.getElementById('forma7')
+
 
 function trocarCor (){
-    forma1.classList.toggle('amarelo')    
+    forma1.classList.toggle('roxo')    
 }
 
 function trocarForma(){
@@ -25,8 +29,23 @@ function meioCirculo(){
     forma4.classList.toggle('meio-circulo')
 }
 
+function mudarTamanho(){
+    forma5.classList.toggle('aumentar')
+}
+
+function mudarTamanho2(){
+    forma6.classList.toggle('diminuir')
+}
+
+function transparente(){
+    forma7.classList.toggle('transparente')
+}
+
 
 forma1.addEventListener('click', trocarCor)
 forma2.addEventListener('click', trocarForma)
 forma3.addEventListener('click', mudarLado)
 forma4.addEventListener('click', meioCirculo)
+forma5.addEventListener('click', mudarTamanho)
+forma6.addEventListener('click', mudarTamanho2)
+forma7.addEventListener('click', transparente)
