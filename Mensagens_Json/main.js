f845fed1c5b98b5d1045ba7911e7a896a338eeac
Vitoria <vazevedo73@gmail.com>
2023-08-30 12:27:20 -0300

    iniciado 'mensagens'

---
+++ Mensagens_Json/main.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const celular = [
+    {
+        nome:
+        imagem:
+        mensagem:
+        contato:
+        tempo:
+        naoLidas:
+        origem:
+    },
+
+    {
+        nome:
+        imagem:
+        mensagem:
+        contato:
+        tempo:
+        naoLidas:
+        origem:
+    },
+
+    {
+        nome:
+        imagem:
+        mensagem:
+        contato:
+        tempo:
+        naoLidas:
+        origem:
+    },
+
+    {
+        nome:
+        imagem:
+        mensagem:
+        contato:
+        tempo:
+        naoLidas:
+        origem:
+    },
+]
