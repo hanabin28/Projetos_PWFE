8942cd2eb43e63ffd1f56625993e71d47cef1d9b
Vitoria <vazevedo73@gmail.com>
2023-09-06 12:27:43 -0300

    finalizando formatação css

--- Mensagens_Json/main.js
+++ Mensagens_Json/main.js
@@ -2,13 +2,12 @@
 
 // const celular = [
 //     {
-//         nome:
-//         imagem:
-//         mensagem:
-//         contato:
-//         tempo:
-//         naoLidas:
-//         origem:
+//         nome: aaaaa (h2)
+//         imagem: ./img/ana.jpg (img)
+//         mensagem: abluble (p)
+//         contato: jajaja (h3)
+//         tempo: 1 dia (h4)
+//         naoLidas: 3 (h5)
 //     },
 
 //     {
@@ -18,7 +17,6 @@
 //         contato:
 //         tempo:
 //         naoLidas:
-//         origem:
 //     },
 
 //     {
@@ -28,7 +26,6 @@
 //         contato:
 //         tempo:
 //         naoLidas:
-//         origem:
 //     },
 
 //     {
@@ -38,6 +35,5 @@
 //         contato:
 //         tempo:
 //         naoLidas:
-//         origem:
 //     },
 // ]
