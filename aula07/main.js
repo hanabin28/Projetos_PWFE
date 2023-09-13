ae426ee14c414e78080e7b41b5d72e1195b577b9
Vitoria <vazevedo73@gmail.com>
2023-09-13 12:09:45 -0300

    iniciado aula07

---
+++ aula07/main.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const spanEndereco = document.getElementById('span-endereco')
+const spanBairro = document.getElementById('span-bairro')
+const spanCidade = document.getElementById('span-cidade')
+const spanEstado = document.getElementById('span-estado')
+const input = document.getElementById('digitar-cep')
+
+async function pegarDados(){
+    
+    const url = 'https://viacep.com.br/ws/01001000/json/'
+
+    if (input != '') {
+        url = 'https://viacep.com.br/ws/' + input + '/json/'
+    }
+
+    const response = await fetch(url)
+    const cep = await response.json()
+    return cep
+
+}
+
+input.addEventListener('blur', pegarDados)
