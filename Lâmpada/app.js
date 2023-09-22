3768b7541fe247b34efe00cd760eab29a96fdc6c
Giseli Cruz <giseli.cruz@PAREXGROUP.CORP>
2023-09-22 01:59:27 -0300

    .

--- Lâmpada/app.js
+++ Lâmpada/app.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const lampImg = document.getElementById('lampada')
+const buttonOn = document.getElementById('btn-lamp-on') 
+const buttonOff = document.getElementById('btn-lamp-off')
+
+function lampOn(){
+    lampImg.src = './img/ligada.jpg'
+}
+
+buttonOn.addEventListener('click', lampOn)
